bf9b0a6fe6b27e3a956a40482f6451c0dfe8c02a
TenTax <Kostya.dorokhov.2016@mail.ru>
2021-04-22 21:37:11 +0300

    #

--- plugins/tentax/contact/routes.php
+++ plugins/tentax/contact/routes.php
@@ -3,5 +3,6 @@ use TenTax\Contact\Components\ContactForm;
 
 Route::post('/send-mail-api-v1.0', function () {
     // return post();
+    // hohj
     return ContactForm::onSend();
 });
--- themes/active/src/js/modules/sendForm.js
+++ themes/active/src/js/modules/sendForm.js
@@ -1,32 +1,53 @@
 import postData from "../services/postData";
 
-const sendForm = () => {
-    const forms = document.querySelectorAll('form');
+const createElement = ({ tagName, classList }) => {
+    const elem = document.createElement(tagName);
+    if (classList) {
+        elem.className = classList;
+    }
+    return elem;
+}
 
-    const loading = () => {
-        console.log('loading');
+const sendForm = () => {
+    const form = document.querySelector('form');
+    const formWrapper = form.parentNode;
+
+    const messageElem = createElement({ tagName: 'div', classList: 'contact-form__cover' });
+
+    const clearInputs = () => {
+        form.reset();
     }
 
-    const showResult = ({ success, error }) => {
-        if (success) {
-            console.log(success);
+    const successOrError = (msg) => {
+        messageElem.innerHTML = msg;
+        setTimeout(() => {
+            messageElem.remove();
+        }, 3000);
+    }
+
+    const setMessage = ({ loading, success, error }) => {
+        if (loading) {
+            formWrapper.appendChild(messageElem);
+            messageElem.innerHTML = loading;
+        } else if (success) {
+            successOrError(success);
+            clearInputs();
         } else if (error) {
-            console.log(error);
+            successOrError(error);
         }
     }
 
-    forms.forEach(form => {
-        form.addEventListener('submit', e => {
-            e.preventDefault();
-            if (true) {
-                loading();
-
-                const data = new FormData(form);
-                postData('/send-mail-api-v1.0', data)
-                    .then(showResult)
-                    .catch();
-            }
-        });
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        if (true) {
+            setMessage({ loading: 'Загрузка' });
+            const data = new FormData(form);
+            postData('/send-mail-api-v1.0', data)
+                .then(setMessage)
+                .catch((err) => {
+                    setMessage({error: err.message});
+                });
+        }
     });
 }
 
